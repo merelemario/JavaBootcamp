f2e307e240e49a142ba31a2015ed4780b9d36a0f
Mario Vera <mariovera097@gmail.com>
2022-06-20 15:29:53 -0400

    finikitao

--- src/main/java/com/roshka/bootcamp/SmallerNumberSameDigits.java
+++ src/main/java/com/roshka/bootcamp/SmallerNumberSameDigits.java
@@ -3,6 +3,33 @@ package com.roshka.bootcamp;
 public class SmallerNumberSameDigits {
     public static long nextSmaller(long n)
     {
-        return n;
+        String number = Long.toString(n);
+        //ordeno 
+        char[] array = number.toCharArray();
+        java.util.Arrays.sort(array);
+        long i=n;
+   
+        while (true){
+            i--;
+
+            if(i<0){
+                return -1;
+            }
+            //paso a cadena para i 
+            String numberi = Long.toString(i);
+
+            //ordeno
+            char[] arrayi = numberi.toCharArray();
+
+            java.util.Arrays.sort(arrayi);
+
+            //comparo
+            if(java.util.Arrays.equals(arrayi, array)){
+                return i;
+            }
+   
+
+        }
+
     }
 }
